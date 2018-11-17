87a6eccad338a57060b9a8f232b619a619cbc484
Arda Kutlu <ardakutlu@gmail.com>
2018-11-17 03:23:40 +0300

    ends

--- conversionScript.py
+++ conversionScript.py
@@ -6,9 +6,14 @@ searchDir = os.path.normpath("E:\\_Sel")
 flvList = [x for x in os.listdir(searchDir) if x.endswith(".flv")]
 
 for x in flvList:
+
     name, ext = os.path.splitext(x)
     fromPath = os.path.join(searchDir,x)
     toPath = os.path.join(searchDir,"%s.mp4" %name)
+    # skip if already converted
+    if os.path.isfile(toPath):
+        print "skipped %s since mp4 version already exists" %x
+        continue
     # print "Sdf", toPath
     print "converting %s" %x
     os.system('ffmpeg.exe -i "{0}" -c:v libx264 -crf 20 -c:a aac -strict -2 "{1}"'.format(fromPath, toPath))

e625f88cd939b70f3196401c598e6461d8f7302a
Arda Kutlu <ardakutlu@gmail.com>
2019-09-24 00:16:01 +0300

    auto add init

--- nearlineBackup/nearlineBackup.py
+++ nearlineBackup/nearlineBackup.py
@@ -21,6 +21,8 @@ class BackupHelper(object):
         self.selfDir = os.path.dirname(os.path.abspath(__file__))
         self.settingsFile = os.path.join(self.selfDir, "backupHelperConfig.json")
         self.settings = self._getSettings()
+        self.settings["autoBackupList"] = self.autoadd()
+
 
     def getdrivefromname(self, name):
         for char in string.ascii_uppercase:
@@ -36,10 +38,22 @@ class BackupHelper(object):
         return
 
     def autoadd(self):
+
+        if not self.projectsRoot:
+            return
+
+        def convertYYMMDD(value):
+            return int(datetime.fromtimestamp(value).strftime('%y%m%d'))
+
         # list the directories
         # allProjects = os.listdir(self.projectsRoot)
-        allProjects = [ os.path.join(self.projectsRoot, name) for name in os.listdir(self.projectsRoot) if os.path.isdir(os.path.join(self.projectsRoot, name)) ]
-        print(sorted(allProjects, key=os.path.getctime))
+        allProjects = [os.path.join(self.projectsRoot, name) for name in os.listdir(self.projectsRoot) if os.path.isdir(os.path.join(self.projectsRoot, name))]
+        filteredProjects = sorted(list(filter(lambda x: convertYYMMDD(os.path.getctime(x)) > self.settings["startDate"], allProjects)),
+                                  key=os.path.getctime)
+
+        if self.maximumNumberOfProjects:
+            filteredProjects = filteredProjects[:-self.maximumNumberOfProjects]
+        return filteredProjects
         # print(allProjects)
         # def takeCreationDate(element):
         #     print(element)
@@ -55,17 +69,23 @@ class BackupHelper(object):
             data = self._loadJson(self.settingsFile)
         except FileNotFoundError:
             data = {"projectsRoot": None,
-                    "backupList": [],
+                    "autoBackupList": [],
+                    "manualBackupList": [],
+                    "startDate": 101010,
                     "targetRoot": None,
                     "minimumDiskSpace": 200,
                     "maximumNumberOfProjects": 0}
             self._dumpJson(data, self.settingsFile)
         return data
 
-    def getBackupList(self):
+    def getAutoBackupList(self):
         """Returns list of projects actively backing up on the nearline folder"""
-        logger.debug("Func: _getProjectList")
-        return self.settings["backupList"]
+        return self.settings["autoBackupList"]
+        pass
+
+    def getManualBackupList(self):
+        """Returns list of projects actively backing up on the nearline folder"""
+        return self.settings["manualBackupList"]
         pass
 
     def getRemainingSpace(self):
@@ -136,15 +156,15 @@ class BackupHelper(object):
                 print("%s is not a valid path" %folderPath)
                 return
 
-        listOfProjects = self.settings["backupList"]
-        listOfProjects.append(folderPath)
-        if int(self.settings["maximumNumberOfProjects"]) > 0 and len(listOfProjects) > int(self.settings["maximumNumberOfProjects"]):
-            if force:
-                oldestNearlineProject = listOfProjects[0]
-                self.removeProject(oldestNearlineProject)
-            else:
-                print("Maximum Number of Projects limit reached. Use -r or --remove flags to remove old projects or increase the limit with -n or --maximumprojects flag\nAborting")
-                return
+        manualBackupList = self.settings["manualBackupList"]
+        manualBackupList.append(folderPath)
+        # if int(self.settings["maximumNumberOfProjects"]) > 0 and len(manualBackupList) > int(self.settings["maximumNumberOfProjects"]):
+        #     if force:
+        #         oldestNearlineProject = manualBackupList[0]
+        #         self.removeProject(oldestNearlineProject)
+        #     else:
+        #         print("Maximum Number of Projects limit reached. Use -r or --remove flags to remove old projects or increase the limit with -n or --maximumprojects flag\nAborting")
+        #         return
         self._dumpJson(self.settings, self.settingsFile)
 
     def removeProject(self, folderPath):
@@ -153,9 +173,9 @@ class BackupHelper(object):
         if os.path.dirname(folderPath) is '': # if the path is not absolute make sure it is
             folderPath = os.path.join(self.projectsRoot, folderPath)
 
-        listOfProjects = self.settings["backupList"]
+        manualBackupList = self.settings["manualBackupList"]
         try:
-            listOfProjects.remove(folderPath)
+            manualBackupList.remove(folderPath)
             backedUpCopy = self.isBackedup(folderPath)
             if backedUpCopy:
                 shutil.rmtree(backedUpCopy)
@@ -181,13 +201,13 @@ class BackupHelper(object):
         if not self.targetRoot:
             print("target root is not defined. use -t or --targetroot flags to define projects root")
             return
-        if not self.getBackupList():
+        if not self.getAutoBackupList() and not self.getManualBackupList():
             print("Backup List is empty. use -a or --add flags to add project folders")
 
         if self.getRemainingSpace() < int(self.minimumDiskSpace):
             if force:
                 # delete the oldest project from the list and give it another go
-                oldestNearlineProject = self.getBackupList()[0]
+                oldestNearlineProject = self.getAutoBackupList()[0]
                 self.removeProject(oldestNearlineProject)
                 backedUpCopy = self.isBackedup(oldestNearlineProject)
                 if backedUpCopy:
@@ -198,6 +218,7 @@ class BackupHelper(object):
                 print("Remaining space at Target Volume is below specified minimum (%s)\n"
                       "Delete some projects from the nearline backup unit or change the minimum disk space tieh -d or --minimumdiskspace flags" %self.minimumDiskSpace)
                 return
+
 
         balFile = self.createBalFile()
 
@@ -219,20 +240,22 @@ class BackupHelper(object):
             return
 
     def createBalFile(self):
-        data = ""
-        backupList = self.getBackupList()
+        data = u""
+        backupList = self.getAutoBackupList()
+        print("here", backupList)
         for x in backupList:
-            line = "{0}\\*.* > {1} /us\n".format(x, os.path.join(self.targetRoot, os.path.basename(x)))
-            data = "%s%s" %(data, line)
+            line = u"{0}\\*.* > {1} /us\n".format(x, os.path.join(self.targetRoot, os.path.basename(x)))
+            data = u"%s%s" %(data, line)
 
         balFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "nearlineBackup.bal")
+        print(data)
 
         if os.path.isfile(balFile):
             backupFile = os.path.join(self.selfDir, "nearlineBackup.bak")
             print(backupFile)
             shutil.copyfile(balFile, backupFile)
             tempFile = "nearlineBackup_TMP.bal"
-            f = open(tempFile, "w+")
+            f = open(tempFile, "w", encoding="utf-8")
             f.write(data)
             f.close()
             shutil.copyfile(tempFile, balFile)
@@ -252,6 +275,7 @@ usage: python nearlineBackup.py [-h] [-f] [-e] [-g]
                                 [--remove <Absolute or Relative Folder Path>]
                                 [--projectsroot <Absolute Path>]
                                 [--targetroot <Absolute Path>]
+                                [--startdate <YYMMDD>
                                 [--drivebyname <String>]
                                 [--minimumdiskspace <integer value>]
                                 [--maximumprojects <integer value>]
@@ -266,6 +290,7 @@ usage: python nearlineBackup.py [-h] [-f] [-e] [-g]
 -p, --projectsroot      defines the source root of projects
 -b, --drivebyname       target drive letter will resolved using this volume name
 -t, --targetroot        defines the target root of nearline backups
+-s, --startdate         starting date for auto adding projects in the projects root
 -d, --minimumdiskspace  defines the minimum disk space required before executing a new backup.
 -n, --maximumprojects   maximum number of projects kept in the nearline unit
 -i, --isbackedup        checks if the given project backed up at the nearline unit. Returns the backup path if exists.                                
@@ -274,7 +299,7 @@ usage: python nearlineBackup.py [-h] [-f] [-e] [-g]
 
     def main(self, args):
         logger.debug("Func: main")
-        opts, remainder = getopt.getopt(args, "a:r:ep:t:d:n:i:b:fhg", ["add=", "remove=", "execute", "projectsroot=", "targetroot=", "minimumdiskspace=", "maximumprojects=", "isbackedup=", "drivebyname=", "force", "help", "getlist"])
+        opts, remainder = getopt.getopt(args, "a:r:ep:t:d:n:i:b:s:fhg", ["add=", "remove=", "execute", "projectsroot=", "targetroot=", "minimumdiskspace=", "maximumprojects=", "isbackedup=", "drivebyname=", "startdate", "force", "help", "getlist"])
 
         if not opts:
             self.executeBackup()
@@ -310,7 +335,7 @@ usage: python nearlineBackup.py [-h] [-f] [-e] [-g]
             if opt in ("-i", "--isbackedup"):
                 print(self.isBackedup(arg))
             if opt in ("-g", "--getlist"):
-                print("\n".join(self.getBackupList()))
+                print("\n".join(self.getAutoBackupList()))
             # if opt in ("-f", "--force"):
             #     force=True
             if opt in ("-e", "--execute"):
